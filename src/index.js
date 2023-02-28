5301b673a86bd274594f2d338a15c3449c9f4f00
monumarquis <monumarquis@gmail.com>
2023-02-28 16:22:58 +0530

    Added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connect = require("./config/db");
+const User = require("./feature/user/user.route")
 const app = express();
 
 const PORT = process.env.PORT || 8001;
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(express.json({limit:"500mb"}));
 app.use(express.urlencoded({ extended: true , limit:"500mb"}));
 
+app.use("/user",User)
 
 app.get("/", (req, res) => {
   res.send("This is  My Es Magic  Backend");
