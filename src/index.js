2ebd463b1e1e06f1e12b6fafcdaf7aee42af9338
monumarquis <monumarquis@gmail.com>
2023-02-28 22:41:29 +0530

    Added

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ const app = express();
 const PORT = process.env.PORT || 8001;
 
 app.use(cors());
-app.use(express.json({limit:"500mb"}));
-app.use(express.urlencoded({ extended: true , limit:"500mb"}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/user",User)
 
